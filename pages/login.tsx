56a957a1bd054c45f9b9e605b0315bda2a430eee
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-05-27 15:59:49 +0500

    test

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SEO from "components/seo";
 import AuthContainer from "containers/auth/auth";
 import LoginForm from "components/loginForm/loginForm";
 import SocialLogin from "components/socialLogin/socialLogin";
+import { GetServerSideProps } from "next";
+import { QueryClient } from "react-query";
+import { getCookie } from "utils/session";
+import { getServerSEOData, SEOData } from "services/restaurantService";
 
 type Props = {};
 
-export default function Login({}: Props) {
+export default function Login({ }: Props) {
   return (
     <>
       <SEO />
@@ -17,3 +21,57 @@ export default function Login({}: Props) {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const queryClient = new QueryClient();
+  const authToken = getCookie("access_token", ctx); // Get the auth token from cookies
+
+
+  const isAuthenticated = !!authToken;
+
+  const [seoData, setSeoData] = useState<SEOData | null>(null);
+
+  useEffect(() => {
+    const fetchSEO = async () => {
+      const domain = window.location.hostname;
+      const localData = {}; // pass real localData if needed
+      const data = await getServerSEOData(domain, localData);
+      setSeoData(data);
+    };
+
+    fetchSEO();
+  }, []);
+
+  console.log("Client SEO Data:", seoData?.restaurant?.id);
+
+
+
+
+  if (isAuthenticated) {
+
+    const domain = window.location.hostname;
+    const allowedDomains = ["yumz.dk", "www.yumz.dk", "localhost"];
+
+    if (!allowedDomains.includes(domain)) {
+      return {
+        redirect: {
+          destination: `/restaurant/${seoData?.restaurant?.id}`,
+          permanent: false,
+        },
+      };
+    }
+    else
+      return {
+        redirect: {
+          destination: "/home", // Or another authenticated route
+          permanent: false,
+        },
+      };
+  }
+
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(queryClient.getQueryCache())),
+    },
+  };
+};
